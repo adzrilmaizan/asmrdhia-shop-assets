a8b26fa0e5ed5f2924ac070aa99a7efc89530def
adzrilmaizan <148976390+adzrilmaizan@users.noreply.github.com>
2026-03-01 12:44:03 +0800

    Refactor admin interceptor for token handling

--- admin-interceptor.js
+++ admin-interceptor.js
@@ -1,45 +1,47 @@
 (function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  const keyFromUrl = urlParams.get('key');
+  const API_HOST = "shopapi.asmrdhia.com";
 
-  if (keyFromUrl) {
-    localStorage.setItem('admin_token', keyFromUrl);
+  // 1) Read key from URL and store
+  const urlParams = new URLSearchParams(window.location.search);
+  const key = urlParams.get("key");
+  if (key) {
+    localStorage.setItem("admin_token", key);
     window.history.replaceState({}, document.title, window.location.pathname);
   }
 
-  const token = localStorage.getItem('admin_token');
-
-  if (!token) {
-    const promptKey = prompt("Masukkan Admin Key:");
-    if (promptKey) {
-      localStorage.setItem('admin_token', promptKey.trim());
-      location.reload();
-    }
-  }
-
+  // 2) Fetch interceptor (only for your API)
   const originalFetch = window.fetch;
   window.fetch = async function (resource, config) {
-    const storedToken = localStorage.getItem('admin_token');
-    if (storedToken) {
-      config = config || {};
-      config.headers = config.headers || {};
-      config.headers['X-Admin-Token'] = storedToken;
-    }
+    try {
+      const token = localStorage.getItem("admin_token");
+      const url = typeof resource === "string" ? resource : (resource?.url || "");
+      const isTarget = url.includes(API_HOST);
+
+      if (token && isTarget) {
+        config = config || {};
+        config.headers = config.headers || {};
+
+        if (config.headers instanceof Headers) config.headers.set("X-Admin-Token", token);
+        else config.headers["X-Admin-Token"] = token;
+      }
+    } catch (e) {}
     return originalFetch(resource, config);
   };
 
-  const origOpen = XMLHttpRequest.prototype.open;
+  // 3) XHR interceptor (already domain-filtered)
+  const originalXhrOpen = XMLHttpRequest.prototype.open;
+  const originalXhrSend = XMLHttpRequest.prototype.send;
+
   XMLHttpRequest.prototype.open = function (method, url) {
     this._url = url;
-    return origOpen.apply(this, arguments);
+    return originalXhrOpen.apply(this, arguments);
   };
 
-  const origSend = XMLHttpRequest.prototype.send;
   XMLHttpRequest.prototype.send = function (body) {
-    const storedToken = localStorage.getItem('admin_token');
-    if (storedToken && this._url && this._url.includes('shopapi.asmrdhia.com')) {
-      this.setRequestHeader('X-Admin-Token', storedToken);
+    const token = localStorage.getItem("admin_token");
+    if (token && this._url && this._url.includes(API_HOST)) {
+      this.setRequestHeader("X-Admin-Token", token);
     }
-    return origSend.call(this, body);
+    return originalXhrSend.call(this, body);
   };
 })();
